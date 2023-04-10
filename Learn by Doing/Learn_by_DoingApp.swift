ac1799f732ca7dde1270ec53df298070c2393a12
Pham Nguyen Phu <57014052+phamnguyenphu@users.noreply.github.com>
2023-04-10 14:47:19 +0700

    Initial Commit

---
+++ Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by Pham Nguyen Phu on 10/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
